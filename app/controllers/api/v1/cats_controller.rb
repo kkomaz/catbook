9850781dcb8a857a9508476b06a5f4beb127fe46
Alex Lee <alex@uniteus.com>
2016-07-10 20:01:43 -0400

    Add hobby_serializer and updates cats and hobby controller

--- app/controllers/api/v1/cats_controller.rb
+++ app/controllers/api/v1/cats_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class CatsController < ApplicationController
       def index
-        render json: Cat.all, include: ['hobbies']
+        render json: Cat.includes(:hobbies), include: ['hobbies']
       end
     end
   end
