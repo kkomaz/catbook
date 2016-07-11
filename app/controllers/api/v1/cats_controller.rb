3dc2c8c7de6c4072737b801ebb8a9f95ff374afa
Alex Lee <alex@uniteus.com>
2016-07-11 01:17:34 -0400

    Clean up middelware issues

--- app/controllers/api/v1/cats_controller.rb
+++ app/controllers/api/v1/cats_controller.rb
@@ -4,6 +4,23 @@ module Api
       def index
         render json: Cat.includes(:hobbies), include: ['hobbies']
       end
+
+      def create
+        attributes = cat_params
+        @cat = Cat.new(attributes)
+
+        if @cat.save
+          render json: @cat, status: :created, location: api_v1_cats_url(@cat)
+        else
+          respond_with_errors(@cat)
+        end
+      end
+
+      private
+
+      def cat_params
+        params.require(:cat).permit(:name, :breed, :weight, :temperament)
+      end
     end
   end
 end
