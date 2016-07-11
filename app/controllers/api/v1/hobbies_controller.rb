ff593740bab9456f03b630f1389de50bef49b335
Alex Lee <alex@uniteus.com>
2016-07-10 20:23:06 -0400

    Hobbies controller

--- app/controllers/api/v1/hobbies_controller.rb
+++ app/controllers/api/v1/hobbies_controller.rb
@@ -1,1 +1,9 @@
-
+module Api
+  module V1
+    class HobbiesController < ApplicationController
+      def index
+        render json: Hobby.includes(:cats), include: ['cats']
+      end
+    end
+  end
+end
